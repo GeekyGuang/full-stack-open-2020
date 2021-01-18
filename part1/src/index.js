5cd7ed7bfa8f636d5fe43c45fd947e0091070602
TravisWongX <travis.x.wong@gmail.com>
2021-01-18 11:32:29 +0800

    React 简介

---
+++ part1/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>greetings</h1>
+      <Hello name="George" age={26 + 10}/>
+      <Hello name={name} age={age}/>
+    </>)
+
+}
+
+ReactDOM.render(<App/> ,
+  document.getElementById('root')
+);
+
